259e0e4a0ced5bb4379ddf60dfe62b7f233f4385
cylrs <44024263+cylrs@users.noreply.github.com>
2020-06-14 00:05:34 +0200

    Add files via upload

--- 3DES/Sequence.cpp
+++ 3DES/Sequence.cpp
@@ -22,7 +22,7 @@ public:
     Sequence(list<int> li);             //OK
     ~Sequence();
     int operator()(int);                //OK
-    int operator[](int);                //OK
+    int& operator[](int);                //OK
     void operator=(long int);           //OK
     Sequence operator*(Sequence);       //OK
     int size();                         //OK
@@ -39,26 +39,27 @@ public:
 Sequence::Sequence(){
     seq.assign(5, experimental::randint(0, 1));
 }
+//CONTSTRUCTEUR AVEC TAILLE DE LA DEQUE
 Sequence::Sequence(int t){
     seq.assign(t, experimental::randint(0, 1));
 }
+//CONSTRUCTEUR PRENNANT UNE LISTE DE DONNEE
 Sequence::Sequence(list<int> li){
     seq.insert(seq.begin(),li.begin(),li.end());
 
 }
+//OPERATOR D ACCES
 int Sequence::operator()(int i) {
     deque<int>::iterator it = seq.begin()+i-1;
     return *it;
 }
-int Sequence::operator[](int i){
+//OPERATOR D ACCES AVEC POSSIBLITE DE MODIFICATION
+int& Sequence::operator[](int i){
     deque<int>::iterator it = seq.begin()+i;
     return *it;
-    //NON COMPREHENSON DE LA QUESTION !
-    //L’opérateur [] quipermettra d’accéder 
-    //(et potentiellement modifier) un bit de la séquence.
 }
+//OPPERATOR QUI PERMET DE TRADUIT UN INT EN UNE SEQUENCE DE BIT PAR AFFECTATION 
 void Sequence::operator=(long int decimal){
-    //VOIR COMMENT REDUIRE LE CODE ET ENLEVER DE TABLEAU QUI EST INUTILE 
     int bits, facteur;
     bits = int(log(decimal)/log(2));
     int binary[bits+1];
@@ -72,11 +73,13 @@ void Sequence::operator=(long int decimal){
     for (int bit : binary) newdeque.insert(newdeque.end(),bit);
     seq.swap(newdeque);
 }
+//METHODE QUI RENVOI LA TAILLE DE DEQUE DE NOTRE SEQUENCE 
 int Sequence::size(){
     int conteur = 0;
     for (auto &i : seq) conteur++;
     return conteur;
 }
+//OPERATOR XOR
 Sequence Sequence::operator*(Sequence s){
     int n = s.size();
     Sequence xorr= s;
@@ -89,6 +92,7 @@ Sequence Sequence::operator*(Sequence s){
     }
     return xorr;
 }
+//METHODE POUR DECALER LES BITS D UNE SEQUENCE DE i POSITION(s) VERS LA GAUCHE 
 void Sequence::decalage(int i){
     int val;
     for (int pos = 0; pos <i; pos++){
@@ -98,11 +102,13 @@ void Sequence::decalage(int i){
         seq.push_back(val);
     }
 }
+//METHODE DE PERMUTATION POUR REMPLACER PAR LES DONNEES DU VECTOR
 Sequence Sequence::permutation(vector<int> v){
     list<int> li;
     li.insert (li.begin (), v.begin (), v.end());
     return Sequence(li);
 }
+//RETURN UNE SOUS SEQUENCE 
 Sequence Sequence::sous_sequence(int debut, int fin){
     list<int> donnee;
     for (int i = debut; i < fin; i++) donnee.push_back(seq[i]);
@@ -142,4 +148,34 @@ string Sequence::afficheT(){
 }
 Sequence::~Sequence(){}
 
+/* **** ICI LES TESTS DES METHODES DEMANDEES AVEC LEURS RESULTATS *******************
+int main(){
+Sequence seqtest = Sequence();
+seqtest.affiche();
+     // RESULTAT : {0  0  0  0  0  }
+seqtest = Sequence(2);
+seqtest.affiche();
+     // RESULTAT : {1  1  }
+seqtest[1]= 0;
+seqtest.affiche();
+     // RESULTAT : {1  0 }
+seqtest= 11;
+seqtest.affiche();
+     // RESULTAT : {1  0  1  1  }
+seqtest.sous_sequence(0,2).affiche();
+     // RESULTAT : {1  0 }
+seqtest.decalage(1);
+seqtest.affiche();
+    //RESULTAT :  {0  1  1  1  }
 
+seqtest.permutation(vector<int> {1,0,0,1}).affiche();
+    //RESULTAT :  {1 0 0 1  }
+Sequence seq1 = Sequence(2);
+seq1 =2; // {1 0}
+seqtest = 3;  // {1 1}
+seq1= seq1*seqtest;
+seq1.affiche();
+    //RESULTAT :  {0 1 }  
+
+} 
+*/
--- 3DES/SequenceD.cpp
+++ 3DES/SequenceD.cpp
@@ -9,175 +9,202 @@
 #include <vector>
 #include "Sequence.cpp"
 using namespace std;
-//ibrahim & Cylia LAST v : 01 06 2020
+//Ibrahim & Cylia LAST v : 01 06 2020
 //TOUTES LES METHODES DE CETTE CLASSE FONCTIONNENT CORRECTEMENT 
-template<class T,int maxsize>
-class SequenceD
+template<int maxsize = 4>
+class SequenceD : public Sequence 
 {
     public:
         SequenceD();
-        SequenceD(T,T);
-        SequenceD(T);
-        void set(T);
+        SequenceD(Sequence,Sequence);
+        SequenceD(Sequence);
+        void set(Sequence);
         int size();
         void affiche();
         void decalageD(int);
-        T left();
-        T right();
+        Sequence left();
+        Sequence right();
         int operator()(int);                //  OK
-        int operator[](int);                //  OK
+        int& operator[](int);                //  OK
         void operator=(long int);           //  OK
         
         SequenceD operator*(SequenceD);
-        T get();
+        SequenceD get();
         void pop();
 
     private:
-        T seqq;
+        Sequence seqL;
+        Sequence seqR;
 
 };
-template<class T,int maxsize>
- SequenceD<T,maxsize>::SequenceD()
+template<int maxsize>
+ SequenceD<maxsize>::SequenceD()
 {
-    seqq = Sequence(maxsize);
+    seqL= Sequence(maxsize/2);
+    seqR= Sequence(maxsize/2);
 }
-template<class T,int maxsize>
- SequenceD<T,maxsize>::SequenceD(T a, T b)  
+template<int maxsize>
+ SequenceD<maxsize>::SequenceD(Sequence l,Sequence r)  
 {
-    list<int> l1= a.toList();
-    list<int> l2= b.toList();
-    l1.merge(l2);
-    seqq=Sequence(l1);
-    cout<<seqq.size()<<'\n';
+    seqL = l;
+    seqR = r;
 }
-template<class T,int maxsize>
- SequenceD<T,maxsize>::SequenceD(T a)  
+template<int maxsize>
+ SequenceD<maxsize>::SequenceD(Sequence a)  
 {
-    seqq=a;
+    int n =a.size();
+    seqL = a.sous_sequence(n/2,n);
+    seqR = a.sous_sequence(0,n/2);
 }
-template<class T, int maxsize>
- void SequenceD<T, maxsize>::set(T a ){
-     seqq=a;
- }
+template<int maxsize> 
+void SequenceD<maxsize>::set(Sequence a){
+    int n =a.size();
+    seqL = a.sous_sequence(n/2,n);
+    seqR = a.sous_sequence(0,n/2);
+}
 
-template<class T,int maxsize>
- int SequenceD<T,maxsize>::size()
+template<int maxsize>
+ int SequenceD<maxsize>::size()
 {
-    seqq.affiche();
-    return seqq.size();
+    return seqL.size()+ seqR.size();
 }
-template<class T,int maxsize>
- void SequenceD<T,maxsize>::affiche(){
-    seqq.affiche();
+template<int maxsize>
+ void SequenceD<maxsize>::affiche(){
+    
+    seqR.affiche();
+    seqL.affiche();
 }
-template<class T,int maxsize>
- void SequenceD<T,maxsize>::decalageD(int n){
-    Sequence lright= (right());
-    Sequence lleft= left();
-    lright.decalage(n);
-    lleft.decalage(n);
-    list<int> ll = lleft.toList();
-    list<int> lr = lright.toList();
-    ll.merge(lr);
-    seqq = Sequence(ll);
+template<int maxsize>
+ void SequenceD<maxsize>::decalageD(int n){
+    seqL.decalage(n);
+    seqR.decalage(n);
 }
-template<class T,int maxsize>
- T SequenceD<T,maxsize>::left()
+template<int maxsize>
+ Sequence SequenceD<maxsize>::left()
+{   return seqL;   
+}
+template<int maxsize>
+ Sequence SequenceD<maxsize>::right()
 {
-    int n =seqq.size();
-    Sequence l = seqq.sous_sequence(n/2,n);
-    return l;
+    return seqR;
 }
-template<class T,int maxsize>
- T SequenceD<T,maxsize>::right()
-{
-int n =seqq.size();
-    Sequence l = seqq.sous_sequence(0,n/2);
-    return l;
+template<int maxsize>
+ int SequenceD<maxsize>::operator()(int i){
+     if (i<seqL.size()) return seqL(i);
+    return seqR(i-seqL.size());
 }
-template<class T, int maxsize>
- int SequenceD<T, maxsize>::operator()(int i){
-    return seqq.operator()(i);
+template<int maxsize>
+ int& SequenceD<maxsize>::operator[](int i){
+     if (i<seqL.size()) return seqL[i];
+    return seqR[i-seqL.size()];
 }
-template<class T, int maxsize>
- int SequenceD<T, maxsize>::operator[](int i){
-    return seqq.operator[](i);
+template<int maxsize>
+ void SequenceD<maxsize>::operator=(long int i){
+     Sequence seqq=i;
+     int sousSeq = seqq.size()/2;
+     seqL = seqq.sous_sequence(0,sousSeq);
+     seqR = seqq.sous_sequence(sousSeq, sousSeq*2);
 }
-template<class T, int maxsize>
- void SequenceD<T, maxsize>::operator=(long int i){
-     seqq.operator=(i);
+template<int maxsize> SequenceD<maxsize>
+ SequenceD<maxsize>::operator*(SequenceD<maxsize> s){
+     SequenceD unionXor;
+     unionXor.seqL = s.seqL* seqL;
+     unionXor.seqR = s.seqR* seqR;
+     return unionXor;
 }
-template<class T, int maxsize> SequenceD<T, maxsize>
- SequenceD<T, maxsize>::operator*(SequenceD<T, maxsize> s){
-     SequenceD newS = SequenceD();
-     newS.seqq = seqq.operator*(s.get());
-     return newS;
-}
-template<class T,int maxsize>
- T SequenceD<T,maxsize>::get()
+/*
+template<int maxsize>
+ SequenceD SequenceD<maxsize>::get()
 {
     return seqq;
 }
 
-template<class T, int maxsize>
- void SequenceD<T, maxsize>::pop(){
+template<int maxsize>
+ void SequenceD<maxsize>::pop(){
      seqq.pop();
  }
 
-
-
-
-void write(SequenceD<Sequence,64> s,std::ostream& outStr){
-    Sequence left = s.left();
+*/
+/*
+template<int maxsize =64>
+ ostream& write(SequenceD<64> s,std::ostream& outStr){
+    /*Sequence left = s.left();
     Sequence right = s.right();
     string txt = left.afficheT() + right.afficheT();
-    outStr.write((txt).c_str(),s.size());
+   return outStr.write((txt).c_str(),s.size());
+      //return outStr;
+   for (int i = 0; i < s.size(); i++) outStr<<s(i);
+   outStr<<endl;
+   return outStr;
 
+
+   
+
+}*/
+
+template<int Size>
+ostream& write(sequenceD<Size> seq , ostream& os )
+{
+	if (Size != 64)
+		return os;
+	os << endl;
+	for (auto i = 0; i < Size; i++)
+	{
+		os << seq(i);
+		if ((i + 1) % 8 == 0) os << " ";
+	}
+	os << endl;
+	return os;
 }
+
+
 
 template<class T>
  void afficheAll(T obj)
 {
-   for (int i =1; i <= obj.size(); i++) cout<<obj (i);
+    obj.affiche();
+   //for (int i =1; i <= obj.size(); i++) cout<<obj (i);
 }
 
-void read(SequenceD<Sequence,64> s,std::istream& inStr){
+void read(SequenceD<64>& s,std::istream& inStr){
     cout<<"inter the sequence of bits one by one";
     //Nous lisons un int mais on cosidere qu'il s'agit d'un char pour la foction qui suit
     char mysequence[64];
     inStr.read(mysequence,s.size());
     list<int> tabtolist;
     for(int i = 0; i<64;i++) tabtolist.push_front(mysequence[i]);
-    s.set(Sequence(tabtolist)); 
+    s.set(Sequence(tabtolist));
 
 }
 
-/*
+
 int main()
 {
-    SequenceD<Sequence,100> cyl;
-    cyl=11;
-    cyl.affiche();
-    vector<int> v = {3,4,1,2,5};
-
-    int tailleV = v.size();
-    int valV= 0;
-    int valS;
-
-
-
-    list<int> li; 
-    for (int i = 0; i < tailleV; i++)
-    {
-        valV = v[i];
-        valV= valV-1;
-        valS =cyl[valV];
-        if (valS>1)valS=0;        
-        li.push_back(valS);
-        cout<<valV<< "  se trouve la valeur suivante  "<<valS<<"  insert  a "<<i<<'\n';
-    }
-    Sequence trans = Sequence(li);
-    trans.affiche();
-
+    Sequence s  =Sequence(64);
+    s =60;
+    auto seqD =SequenceD<64> (s);
+    seqD.affiche();
+    //RESULTAT  {1  1  1  }  {1  0  0  }
+    seqD.decalageD(1);
+    seqD.affiche();
+    //RESULTAT  {1  1  1  }  {0  0  1  }
+    seqD.left().affiche();
+    //RESULTAT {0  0  1  }
+    seqD.right().affiche();
+    //RESULTAT {1  1  1  }
+    cout<<seqD.size(); 
+    //RESULTAT : 6 
+    afficheAll(seqD);
+    //RESULTAT {1  1  1  }  {0  0  1  }
+    cout<<seqD(1); //AFFICHE 0 DONC ok 
+    seqD[1]=1;
+    afficheAll(seqD);
+    // RESULTAT {0  1  1  } {0  1  1  }
+    seqD = seqD*seqD;
+    seqD.affiche();
+    // RESULTAT {0  0  0  } {0  0  0  }
+    cout<<"afcchagggge test write";
+    cout << "WRITE : " << endl;
+    write(seqD,cout);
     
-}*/
+}
