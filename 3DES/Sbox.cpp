4dc6b08da9f7ca99bcce7b1411c4b0c46d12a15c
cylrs <44024263+cylrs@users.noreply.github.com>
2020-06-15 13:55:07 +0200

    Add files via upload

--- 3DES/Sbox.cpp
+++ 3DES/Sbox.cpp
@@ -5,32 +5,39 @@
 #include <list>
 #include <iterator>
 #include <vector>
-#include "Sequence.cpp"
+#include "SequenceD.cpp"
 
-
-//ibrahim & Cylia LAST v : 01 06 2020
-//TOUTES LES METHODES DE CETTE CLASSE FONCTIONNENT CORRECTEMENT 
+//Ibrahim & Cylia LAST v : 14 06 2020
+//TOUTES LES METHODES DE CETTE CLASSE FONCTIONNENT CORRECTEMENT
 
 class Sbox
 {
 private:
-    Sequence **box;
+    //UN TAB DE INT VA ETRE PLUS SIMPLE A MANIPULER COMME INDIQUE DANS LE DESCRIPTIF
+    int **box;
+
 public:
     Sbox(int **);
-    Sbox(Sequence **);
+    Sbox(Sbox &);
     ~Sbox();
-    Sbox operator=(Sbox);
+    Sbox &operator=(Sbox &);
+    int getValue(int, int);
     Sequence operator()(Sequence);
 };
-int todecimal(Sequence a){
-    int  rem, temp, dec = 0, b = 1;
+//cette methode nous permet de passer de bits vers decimals
+int todecimal(Sequence a)
+{
+    int rem, temp, dec = 0, b = 1;
     int bin = 0;
     //Transformation en chiffre
-    while (a.size()<6) a.push();
-    
-    for (int i = 0; i <a.size(); i++)bin+=a[i] *pow(10,a.size()-i-1);
+    while (a.size() < 6)
+        a.push();
+
+    for (int i = 0; i < a.size(); i++)
+        bin += a[i] * pow(10, a.size() - i - 1);
     temp = bin;
-    while (temp > 0){
+    while (temp > 0)
+    {
         rem = temp % 10;
         dec = dec + rem * b;
         b *= 2;
@@ -39,77 +46,62 @@ int todecimal(Sequence a){
     return dec;
 }
 
-Sbox::Sbox(int ** tabInitial)
+Sbox::Sbox(int **tabInitial)
 {
-    //ALLOCATION DE MEMOIRE 
-    box = new Sequence*[16*4];
-    Sequence nul =0;
-    box[0] = new Sequence[16*4];
-    for(int i=1;i<4;i++)box[i]=box[i-1]+16;
-    //TRANSFORMATION DE CHAQUE CASE DU TAB 2D
-     for(int i=0;i<4;i++)
-    {
-        for(int j=0;j<16;j++){
-            Sequence a=tabInitial[i][j];
-            int row,col;
-            list<int> rowl,coll,val;
-            rowl.push_front(a[5]);
-            rowl.push_front(a[0]);
-            coll.push_front(a[4]);
-            coll.push_front(a[3]);
-            coll.push_front(a[2]);
-            coll.push_front(a[1]);
-            val.push_front(a[5]);
-            val.push_front(a[4]);
-            val.push_front(a[3]);
-            val.push_front(a[2]);
-            row = todecimal(Sequence(rowl));
-            col = todecimal(Sequence(coll));
-            box[row][col]= Sequence(val);
-        }
-    }
-
-        
+    //ALLOCATION DE MEMOIRE
+    box = new int *[16 * 4];
+    Sequence nul = 0;
+    box[0] = new int[16 * 4];
+    for (int i = 1; i < 4; i++)
+        box[i] = box[i - 1] + 16;
+    //affectation
+    box = tabInitial;
 }
 //CONSTRUCTEUR DE RECOPIE
-Sbox::Sbox(Sequence ** tabInitial){
-    //ALLOCATION DE MEMOIRE 
-    box = new Sequence*[16*4];
-    Sequence nul =0;
-    box[0] = new Sequence[16*4];
-    for(int i=1;i<4;i++)box[i]=box[i-1]+16;
-    for(int i=0;i<4;i++)
-    {
-        for(int j=0;j<16;j++){
-           box[i][j]=tabInitial[i][j]; 
-        }
-    }
+Sbox::Sbox(Sbox &newBox)
+{
+    box = newBox.box;
 }
 
-Sbox Sbox::operator=(Sbox s){
-    return Sbox(s.box);
+Sbox &Sbox::operator=(Sbox &s)
+{
+    return s;
 }
+int Sbox::getValue(int row, int col)
+{
+    return box[row][col];
+}
+Sequence Sbox::operator()(Sequence a)
+{
 
-Sequence Sbox::operator()(Sequence a){
-    int row,col;
-    list<int> rowl,coll,val;
+    int row, col;
+    list<int> rowl, coll;
+    Sequence result = Sequence(4);
+    // on recupere la ligne
     rowl.push_front(a[5]);
     rowl.push_front(a[0]);
+    // on recupere la colonne
     coll.push_front(a[4]);
     coll.push_front(a[3]);
     coll.push_front(a[2]);
     coll.push_front(a[1]);
-    val.push_front(a[5]);
-    val.push_front(a[4]);
-    val.push_front(a[3]);
-    val.push_front(a[2]);
+    //convertion en entier
+    row = a(5) * 2 + a(5) * 4;
     row = todecimal(Sequence(rowl));
     col = todecimal(Sequence(coll));
-    return Sequence(val);
+    const auto val = getValue(row, col);
+    result = val;
+    return result;
 }
 
 Sbox::~Sbox()
 {
-    delete [] box[0];
-    delete [] box;
+
+    delete[] box[0];
+    delete[] box;
 }
+/*
+int main()
+{
+    
+}*/
