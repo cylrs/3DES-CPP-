3df3afc3f52f55285fab60af65d26542a50455a1
cylrs <44024263+cylrs@users.noreply.github.com>
2020-06-15 14:09:58 +0200

    Add files via upload

--- 3DES/S_function.cpp
+++ 3DES/S_function.cpp
@@ -4,31 +4,46 @@
 class S_function
 {
 private:
-    Sbox * Sboxes;
+    int*** Sboxes;
 public:
     S_function(int ***);
-    S_function(Sbox *);
-    S_function operator=(S_function);
+    S_function();
+    S_function(S_function&);
+    S_function& operator=(S_function&);
     Sequence operator()(Sequence);
     ~S_function();
 };
 
 S_function::S_function(int *** tabInitial){
-    for(int x = 0; x < 8; ++x){
-        Sbox a(tabInitial[x]);
-        Sboxes[x] = a;
-    }   
+    Sboxes = tabInitial;
+}
+//CONSTRUCTEUR DE RECOPIE 
+S_function::S_function(S_function & s)
+{
+    Sboxes = s.Sboxes;
 }
 
-S_function::S_function(Sbox * s ){
-    Sboxes = s;
+S_function& S_function::operator=(S_function& s) {
+    return s;
 }
 
-S_function S_function::operator=(S_function s) {
-    return S_function(s.Sboxes);
-}
 
-S_function::~S_function(){
-    for(int x = 0; x < 8; ++x) Sboxes[x].~Sbox();
+Sequence S_function::operator()(Sequence seq){
+    Sequence res;
+    return res;
 }
-
+//  http://sdz.tdct.org/sdz/ableaux-pointeurs-et-allocation-dynamique.html
+S_function::~S_function()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 64; j++)
+        {
+            free(Sboxes[i][j]);
+        }
+        free(Sboxes[i]);
+    }
+    free(Sboxes);
+    //delete[] Sboxes[0];
+    //delete Sboxes;
+}
